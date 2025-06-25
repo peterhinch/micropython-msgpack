ec70b9fb60eb9bc6cc93763ebe82754df16a8f2d
Peter Hinch <peter@hinch.me.uk>
2025-06-25 14:09:27 +0100

    mp_dump.py: Replace despatch table with code.

--- umsgpack/mp_dump.py
+++ umsgpack/mp_dump.py
@@ -198,26 +198,24 @@ def mpdump(obj, fp, options):
         pass
     # Is obj a built-in type?
     # NOTE: using a depatch table consumed 1800 bytes more than code below.
+    ok = True
     if isinstance(obj, bool):
         _pack_boolean(obj, fp)
-        return
-    if isinstance(obj, int):
+    elif isinstance(obj, int):
         _pack_integer(obj, fp)
-        return
-    if isinstance(obj, float):
+    elif isinstance(obj, float):
         _pack_float(obj, fp, options)
-        return
-    if isinstance(obj, str):
+    elif isinstance(obj, str):
         _pack_string(obj, fp)
-        return
-    if isinstance(obj, (bytes, bytearray)):
+    elif isinstance(obj, (bytes, bytearray)):
         _pack_binary(obj, fp)
-        return
-    if isinstance(obj, (list, tuple)):
+    elif isinstance(obj, (list, tuple)):
         _pack_array(obj, fp, options)
-        return
-    if isinstance(obj, (dict, OrderedDict)):
+    elif isinstance(obj, (dict, OrderedDict)):
         _pack_map(obj, fp, options)
+    else:
+        ok = False
+    if ok:
         return
 
     if not custom:  # Custom class is last chance saloon.
